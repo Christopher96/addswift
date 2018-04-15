f18fc00bf8b16556a4f1337bb35f04293d278bc5
syphez <syphez@localhost>
2018-04-15 18:41:54 +0200

    ?

--- client/plugins/vuetify.js
+++ client/plugins/vuetify.js
@@ -2,4 +2,14 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import colors from 'vuetify/es5/util/colors'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+    theme: {
+        primary: "#FF1744",
+        secondary: "#C2185B",
+        accent: "#66BB6A",
+        error: "#f44336",
+        warning: "#ffeb3b",
+        info: "#2196f3",
+        success: "#4caf50"
+      }
+})
